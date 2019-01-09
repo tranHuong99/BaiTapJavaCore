957802bf5d8dfc8db361eaedfce54de60791fc4d
tranHuong99 <tranhuong.hvact@gmail.com>
2019-01-10 00:00:52 +0700

    Thong bao

--- BaiTapSlide7/src/baitapslide7/SmartPhone.java
+++ BaiTapSlide7/src/baitapslide7/SmartPhone.java
@@ -9,6 +9,30 @@ package baitapslide7;
  *
  * @author HUONG XINH GAI
  */
-public class SmartPhone {
-    
+public class SmartPhone extends Product{
+    private boolean hasCamera;
+    private int sim;
+    public boolean getHasCamera(){
+        return true;
+    }
+    public void setHasCamera(boolean hasCamera){
+        this.hasCamera = hasCamera;
+    }
+    public int getSin(){
+        return sim;
+    }
+    public void setSim(int sim){
+        this.sim = sim;
+    }
+    public SmartPhone(){
+        
+    }
+    public SmartPhone(int add, String ten, double cost, int amount, boolean hasCm, int s_i_m){
+        super(add);
+        super(ten);
+        super(cost);
+        super(amount);
+        hasCamera = hasCm;
+        sim = s_i_m;
+    }
 }
